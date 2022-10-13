deb39c0d68efc3443299c92f4d3d419ea03eda9c
Felipe Flores <felipe.flores_external@wartsila.com>
2022-10-13 09:43:18 +0200

    Introduce in memory repository instead of mock

--- obscure-tests/src/main/java/eu/qwan/exercises/dirtytests/obscure/domain/Transport.java
+++ obscure-tests/src/main/java/eu/qwan/exercises/dirtytests/obscure/domain/Transport.java
@@ -1,34 +1,12 @@
 package eu.qwan.exercises.dirtytests.obscure.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class Transport {
-  private TransportOrganisation owner;
-  private String                transportReferenceNumber;
+  private final TransportOrganisation owner;
+  private final String transportReferenceNumber;
   private TransportOrganisation carrier;
-
-  public TransportOrganisation getCarrier() {
-    return carrier;
-  }
-
-  public void setCarrier(TransportOrganisation carrier) {
-    this.carrier = carrier;
-  }
-
-  public TransportOrganisation getOwner() {
-    return owner;
-  }
-
-  public void setOwner(TransportOrganisation owner) {
-    this.owner = owner;
-  }
-
-  public String getTransportReferenceNumber() {
-    return transportReferenceNumber;
-  }
-
-  public void setTransportReferenceNumber(String transportReferenceNumber) {
-    this.transportReferenceNumber = transportReferenceNumber;
-  }
 }
