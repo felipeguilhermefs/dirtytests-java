ad15df6e0e89d5fd77c686a53622979f9f65f964
Felipe Flores <felipe.flores_external@wartsila.com>
2022-10-13 23:05:24 +0200

    Split test cases to test a single thing

--- obscure-tests/src/main/java/eu/qwan/exercises/dirtytests/obscure/repositories/InMemoryOrganisationRepository.java
+++ obscure-tests/src/main/java/eu/qwan/exercises/dirtytests/obscure/repositories/InMemoryOrganisationRepository.java
@@ -1,11 +1,19 @@
 package eu.qwan.exercises.dirtytests.obscure.repositories;
 
 import eu.qwan.exercises.dirtytests.obscure.domain.TransportOrganisation;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InMemoryOrganisationRepository implements OrganisationRepository {
 
+    private final Map<String, TransportOrganisation> organisations = new HashMap<>();
+
     @Override
     public TransportOrganisation findByOrn(String orn) {
-        return null;
+        return organisations.get(orn);
+    }
+
+    public void save(String orn, TransportOrganisation organisation) {
+        organisations.put(orn, organisation);
     }
 }
