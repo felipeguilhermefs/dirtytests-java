67b8acdc1fd182e85bee98fea83d124543395f79
Felipe Flores <felipe.flores_external@wartsila.com>
2022-10-14 11:48:24 +0200

    Stop using mock for user repository

--- misplaced-mocks/src/test/java/eu/qwan/dirtytest/mocks/UserPasswordResetTest.java
+++ misplaced-mocks/src/test/java/eu/qwan/dirtytest/mocks/UserPasswordResetTest.java
@@ -1,5 +1,8 @@
 package eu.qwan.dirtytest.mocks;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -9,14 +12,19 @@ import static org.mockito.Mockito.*;
 public class UserPasswordResetTest {
 
     EmailFactory emailFactory = mock(EmailFactory.class);
-    UserRepository userRepository = mock(UserRepository.class);
+    InMemoryUserRepository userRepository = spy(new InMemoryUserRepository());
     Email email = mock(Email.class);
     Mailer mailer = mock(Mailer.class);
     User user = mock(User.class);
 
+    @BeforeEach
+    void setup() {
+        when(user.getId()).thenReturn("user-id");
+        userRepository.add(user);
+    }
+
     @Test
     public void sendNotification() {
-        when(userRepository.byId("user-id")).thenReturn(Optional.of(user));
         when(user.getEmailAddress()).thenReturn("user@company.com");
         when(emailFactory.create(eq("user@company.com"), eq("PASSWORD_RESET"), any())).thenReturn(email);
         when(email.send(mailer)).thenReturn(true);
@@ -25,12 +33,10 @@ public class UserPasswordResetTest {
 
         verify(email).send(any());
         verify(emailFactory).create(any(), any(), any());
-        verify(userRepository).byId("user-id");
     }
 
     @Test
     public void testNotificationFails() {
-        when(userRepository.byId("user-id")).thenReturn(Optional.of(user));
         when(user.getEmailAddress()).thenReturn("user@company.com");
         when(emailFactory.create(eq("user@company.com"), eq("PASSWORD_RESET"), any())).thenReturn(email);
         when(email.send(mailer)).thenReturn(false);
@@ -45,16 +51,30 @@ public class UserPasswordResetTest {
         verify(emailFactory, times(2)).create(any(), any(), any());
         verify(userRepository).byId("user-id");
     }
+
     @Test
     public void userNotFound() {
-        when(userRepository.byId("user-id")).thenReturn(Optional.empty());
         when(email.send(mailer)).thenReturn(false);
         when(emailFactory.create("servicedesk@qwan.eu", "SD", null)).thenReturn(email);
         PasswordResetController ctrl = new PasswordResetController(emailFactory, userRepository, mailer);
-        ctrl.resetPassword("user-id");
+        ctrl.resetPassword("user-id2");
 
         verify(email).send(any());
         verify(emailFactory).create(any(), any(), any());
-        verify(userRepository).byId("user-id");
+        verify(userRepository).byId("user-id2");
+    }
+
+    static class InMemoryUserRepository implements UserRepository {
+
+        Map<String, User> users = new HashMap<>();
+
+        @Override
+        public Optional<User> byId(String id) {
+            return Optional.ofNullable(users.get(id));
+        }
+
+        public void add(User user) {
+            users.put(user.getId(), user);
+        }
     }
 }
