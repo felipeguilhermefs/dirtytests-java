4a5ed7df6492199359acbe719e1ebcf6a18f8983
Felipe Flores <felipe.flores_external@wartsila.com>
2022-10-14 23:51:54 +0200

    Remove unecessary type params

--- misplaced-mocks/src/main/java/eu/qwan/dirtytest/mocks/EmailFactory.java
+++ misplaced-mocks/src/main/java/eu/qwan/dirtytest/mocks/EmailFactory.java
@@ -6,12 +6,12 @@ import java.util.Map;
 public class EmailFactory {
     private static final String SENDER = "info@qwan.eu";
 
-    private final Map<String, String> bodies = new HashMap<String, String>() {{
+    private final Map<String, String> bodies = new HashMap<>() {{
         put("PASSWORD_RESET", "Hello %s, \nPlease click this link to reset your password: %s\nKind regards,\nQWAN");
         put("SD", "User %s wants to reset his/her password, but sending the email failed");
     }};
 
-    private final Map<String, String> subjects = new HashMap<String, String>() {{
+    private final Map<String, String> subjects = new HashMap<>() {{
         put("PASSWORD_RESET", "Password reset");
         put("SD", "Problem notification");
     }};
